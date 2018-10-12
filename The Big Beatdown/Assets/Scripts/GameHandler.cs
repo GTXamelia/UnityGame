e0fe8b0a39d398970cd03ae8054e8aeddd541ba4
Cian Gannon <cianmagfhionnain@outlook.com>
2018-10-12 16:55:35 +0100

    Added basic collision detection to player

---
+++ The Big Beatdown/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log("Test");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ The Big Beatdown/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate()
+	{
+		float moveHotizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector2 movement = new Vector2(moveHotizontal, moveVertical);
+
+		rb2d.AddForce(movement * speed);
+	}
+}
