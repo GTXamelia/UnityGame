8dda8532d778305b94c6750b2e33d8bdc3d1f461
Cian Gannon <cianmagfhionnain@outlook.com>
2018-11-09 14:06:33 +0000

    Added one time trigger

--- The Big Beatdown/Assets/Scripts/Trigger1.cs
+++ The Big Beatdown/Assets/Scripts/Trigger1.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class Trigger1 : MonoBehaviour {
 
+	bool activated;
+
 	void Awake()
 	{
-		BoxCollider2D bc;
-		bc = gameObject.AddComponent<BoxCollider2D>() as BoxCollider2D;
-		bc.size = new Vector2(3.0f, 1.0f);
-		bc.isTrigger = true;
 
-		gameObject.transform.localScale = new Vector3(3.0f, 1.0f, 1.0f);
-		gameObject.transform.position = new Vector3(0.0f, -2.0f, 0.0f);
 	}
 
 	void OnTriggerEnter2D(Collider2D col)
 	{
-		Debug.Log("GameObject2 collided with " + col.name);
+		Debug.Log("Trigger1 collided with " + col.name + " " + this.activated);
+
+		if (this.activated == false)
+		{
+			Debug.Log("Spawn enemies");
+
+			this.activated = true;
+		}
+		else
+		{
+			Debug.Log("Already triggered :" + this.activated);
+		}
+
 	}
 }
