8be732bdcd4218c35e200d8d895751dc5163b1c0
Cian Gannon <cianmagfhionnain@outlook.com>
2018-11-14 00:43:35 +0000

    Added enemy movement

---
+++ The Big Beatdown/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+	public float speed;
+	public Transform Player;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 displacement = Player.position - transform.position;
+		displacement = displacement.normalized;
+		if (Vector2.Distance(Player.position, transform.position) > 2.0f)
+		{
+			transform.position += (displacement * speed * Time.deltaTime);
+
+		}
+		else
+		{
+			//call hit player method
+		}
+
+	}
+
+}
