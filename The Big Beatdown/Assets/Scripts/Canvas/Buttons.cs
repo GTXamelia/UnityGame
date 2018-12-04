25a4a52e13c564f38defd2a7750f688e6224e3bc
Cian Gannon <cianmagfhionnain@outlook.com>
2018-12-04 20:47:22 +0000

    Added touch buttons

---
+++ The Big Beatdown/Assets/Scripts/Canvas/Buttons.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour {
+
+	public void Level1()
+	{
+		Debug.Log("test");
+	}
+}
