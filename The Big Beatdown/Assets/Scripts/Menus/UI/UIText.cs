405c356b85460f5c1f86b28b59f2d6aac762619d
Cian Gannon <cianmagfhionnain@outlook.com>
2018-12-05 14:51:06 +0000

    Added score text to UI

--- The Big Beatdown/Assets/Scripts/Menus/UI/UIText.cs
+++ The Big Beatdown/Assets/Scripts/Menus/UI/UIText.cs
@@ -6,8 +6,15 @@ using UnityEngine.UI;
 public class UIText : MonoBehaviour {
 
 	public Text timerLabel;
+	public Text scoreLabel;
 
 	private float time;
+	private int scoreAmount = 0;
+
+	void Start()
+	{
+		scoreLabel.text = string.Format("Score:{0}", scoreAmount);
+	}
 
 	void Update()
 	{
@@ -19,5 +26,12 @@ public class UIText : MonoBehaviour {
 
 		//update the label value
 		timerLabel.text = string.Format("{0:00} : {1:00} : {2:000}", minutes, seconds, fraction);
+	}
+
+	public void score(int amount)
+	{
+		scoreAmount += amount;
+
+		timerLabel.text = string.Format("{0}", scoreAmount);
 	}
 }
