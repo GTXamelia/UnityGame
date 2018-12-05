a2892d58a423289823f2bdad6122d0998837985d
Cian Gannon <cianmagfhionnain@outlook.com>
2018-12-05 14:35:55 +0000

    Added timer

---
+++ The Big Beatdown/Assets/Scripts/Menus/UI/UIText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIText : MonoBehaviour {
+
+	public Text timerLabel;
+
+	private float time;
+
+	void Update()
+	{
+		time += Time.deltaTime;
+
+		float minutes = time / 60; //Divide the guiTime by sixty to get the minutes.
+		float seconds = time % 60; //Use the euclidean division for the seconds.
+		float fraction = (time * 100) % 100;
+
+		//update the label value
+		timerLabel.text = string.Format("{0:00} : {1:00} : {2:000}", minutes, seconds, fraction);
+	}
+}
