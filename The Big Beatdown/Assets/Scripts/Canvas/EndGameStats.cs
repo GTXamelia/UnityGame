1a5da14cef9125a7b252e7569931cda36a6672e9
Cian Gannon <cianmagfhionnain@outlook.com>
2018-12-06 16:23:11 +0000

    Added end game stats to end of game scene

---
+++ The Big Beatdown/Assets/Scripts/Canvas/EndGameStats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGameStats : MonoBehaviour
+{
+	public Text scoreLabel;
+	public Text healthLabel;
+	public Text spcecialLabel;
+
+	// Use this for initialization
+	void Start ()
+	{
+		scoreLabel.text = string.Format("Score:{0}", PlayerParams.score);
+		healthLabel.text = string.Format("Health:{0}", PlayerParams.health);
+		spcecialLabel.text = string.Format("Special:{0}", PlayerParams.special);
+	}
+}
