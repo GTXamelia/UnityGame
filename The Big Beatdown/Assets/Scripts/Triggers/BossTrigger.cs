c7ed2206e032b3e898e9c22197815f0fe5b98eb0
Cian Gannon <cianmagfhionnain@outlook.com>
2018-12-06 19:17:58 +0000

    Added trigger comments

--- The Big Beatdown/Assets/Scripts/Triggers/BossTrigger.cs
+++ The Big Beatdown/Assets/Scripts/Triggers/BossTrigger.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class BossTrigger : MonoBehaviour
 {
+	// Public variable
 	public GameObject BossCollider;
 
+	// Ran once
 	void start()
 	{
-		BossCollider.SetActive(true);
+		BossCollider.SetActive(true); // Activate boss Collider
 	}
 
+	// Collision detection
 	void OnTriggerEnter2D(Collider2D col)
 	{
-		BossCollider.SetActive(false);
+		BossCollider.SetActive(false); // Set boss Collider to inactive (Boss is free to move)
 	}
 }
--- The Big Beatdown/Assets/Scripts/Triggers/Trigger.cs
+++ The Big Beatdown/Assets/Scripts/Triggers/Trigger.cs
@@ -4,56 +4,64 @@ using UnityEngine;
 
 public class Trigger : MonoBehaviour
 {
+	// Private variables
 	private bool activated;
 	private GameObject clone;
 	private GameObject[] OBCount;
 
+	// Public variables
 	public GameObject enemy;
 	public Transform[] spawnPoints;
 	public GameObject ColliderLeft;
 	public GameObject ColliderRight;
 
+	// Ran once
 	void Awake()
 	{
-		OBCount = GameObject.FindGameObjectsWithTag("Enemies");
+		OBCount = GameObject.FindGameObjectsWithTag("Enemies"); // Get all objects with tag 'Enemies'
 	}
 
+	// Ran each frame
 	void Update()
 	{
-		OBCount = GameObject.FindGameObjectsWithTag("Enemies");
+		OBCount = GameObject.FindGameObjectsWithTag("Enemies"); // Get all objects with tag 'Enemies'
 
-		if(OBCount.Length <= 0)
+		if (OBCount.Length <= 0) // If array is less than or equal to zero
 		{
+			// Deactivate colliders
 			ColliderLeft.SetActive(false);
 			ColliderRight.SetActive(false);
 		}
 		else
 		{
+			// Deactivate colliders
 			ColliderLeft.SetActive(true);
 			ColliderRight.SetActive(true);
 		}
 	}
 
+	// Collision detection
 	void OnTriggerEnter2D(Collider2D col)
 	{
-		if (this.activated == false)
+		if (this.activated == false) // While this is inactive
 		{
-			StartCoroutine(SpawnAsync());
+			StartCoroutine(SpawnAsync()); // Run async function to spawn enemies
 
-			this.activated = true;
+			this.activated = true; // activate object
 		}
 	}
 
+	// Async function responisble for spawning enemies
 	IEnumerator SpawnAsync()
 	{
-		for (int i = 0; i < 5; i++)
+		for (int i = 0; i < 5; i++) // Runs 5 times for a total of 10 enemies
 		{
-			clone = Instantiate(enemy, spawnPoints[0].position, transform.rotation);
-			clone.tag = "Enemies";
-			clone = Instantiate(enemy, spawnPoints[1].position, transform.rotation);
-			clone.tag = "Enemies";
+			clone = Instantiate(enemy, spawnPoints[0].position, transform.rotation); // Clone enemy object
+			clone.tag = "Enemies"; // Set tag to Enemies
+			clone = Instantiate(enemy, spawnPoints[1].position, transform.rotation); // Clone enemy object
+			clone.tag = "Enemies";// Set tag to Enemies
 
-			yield return new WaitForSeconds(5.0f);
+			yield return new WaitForSeconds(3.0f); // Wait 3 seconds
 		}
 	}
 }
