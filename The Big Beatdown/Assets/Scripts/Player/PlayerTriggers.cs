846938c9e9c9820551e5efc0b29a1430df3bd29e
Cian Gannon <cianmagfhionnain@outlook.com>
2018-12-06 17:13:36 +0000

    Fixed boss damage bug

--- The Big Beatdown/Assets/Scripts/Player/PlayerTriggers.cs
+++ The Big Beatdown/Assets/Scripts/Player/PlayerTriggers.cs
@@ -6,7 +6,7 @@ public class PlayerTriggers : MonoBehaviour
 {
 	void OnTriggerEnter2D(Collider2D col)
 	{
-		if (col.gameObject.tag == "Enemies")
+		if (col.gameObject.tag == "Enemies" || col.gameObject.tag == "Boss")
 		{
 			EnemyHealth enemyHealth = col.GetComponent<EnemyHealth>();
 			enemyHealth.TakeDamage(10);
