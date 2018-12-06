4cfd9e8107ef9ec5fa8a09fff359ae3fc6f4bbd9
Cian Gannon <cianmagfhionnain@outlook.com>
2018-12-06 19:19:38 +0000

    Commented static class

--- The Big Beatdown/Assets/Scripts/PlayerParams.cs
+++ The Big Beatdown/Assets/Scripts/PlayerParams.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerParams
 {
+	// Static variables used to store data between levels
 	static public int health = 0;
 	static public int score = 0;
 	static public int special = 0;
